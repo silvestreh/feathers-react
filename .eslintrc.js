bfd942325b8165c2138c0372af49d640950fe08d
Silvestre Herrera <hello@silvestre.io>
2021-11-10 16:01:35 -0300

    Update dependencies / eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'semistandard'
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {}
+};
